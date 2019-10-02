d4535d86a7d2e0b2c7b4a5069b0dce3e2b6822d5
Simon Grosse-Holz <sgh256@mit.edu>
2019-10-02 17:48:05 -0400

    version 0.1.0

--- nglutils/nglutils.py
+++ nglutils/nglutils.py
@@ -25,92 +25,106 @@ import numpy as np
 # specific things
 import mdtraj
 import nglview
-# custom things
-import openmmlib
 
 # ----------- Handling of hexcolors -----------------
 
-def color_as_hexstring(val, cmap=plt.cm.viridis, include_alpha=False):
+def hexscale_from_cmap(cmap, N):
     """
-    Maps a scalar in [0, 1] to a color and represents it as a string, e.g.
-    0xff440154 (for viridis(0))
-    """
-    col = cmap(float(max(0, min(val, 1)))) # Clamping val to [0, 1]
-    if include_alpha:
-        return "0x{3:02x}{0:02x}{1:02x}{2:02x}".format(int(255*col[0]),
-                                                       int(255*col[1]),
-                                                       int(255*col[2]),
-                                                       int(255*col[3]))
-    else:
-        return "0x{0:02x}{1:02x}{2:02x}".format(int(255*col[0]), int(255*col[1]), int(255*col[2]))
-    
-def hexcolors_polymers(lengths, maps=[plt.cm.viridis]):
-    """
-    Generate a list of colors for use in nglview.NGLWidget._set_color_by_residue.
-    The idea is to patch several colormaps (or the same one multiple times),
-    one for each chain.
-    
+    Evaluate a colormap at N points.
+
     Parameters
     ----------
-    lengths : np.array
-        lengths of the polymer chains (no. of residues in mdtraj language)
-    maps : list of executable objects
-        list of plt.cm colormaps to be cycled through
-        Default: [plt.cm.viridis]
-    """
-    lengths = np.cumsum(np.insert(np.array(lengths), 0, 0))
-    
-    def chainNo(res):
-        return np.min(np.argwhere(lengths > res))
-    def pos_in_chain(res):
-        chain = chainNo(res)
-        return (res - lengths[chain-1])/(lengths[chain]-1 - lengths[chain-1])
-    def cmap(res):
-        return maps[(chainNo(res)-1) % len(maps)]
-    
-    return [color_as_hexstring(pos_in_chain(res), cmap=cmap(res)) for res in range(lengths[-1])]
+    cmap : function
+        a function taking a scalar value between 0 and 1 and giving a color as
+        rgb(a) with values between 0 and 1. These are for example the pyplot
+        colormaps, like plt.cm.viridis
+    N : int
+        number of steps on which to evaluate the colormap
 
-# ------------ Loading stuff from disk ----------------
-
-def load_traj_from_path(path, mon):
+    Returns
+    -------
+    scale : a list of numbers representing the colors from the map, written in
+        the format 0xrrggbb
     """
-    Load trajectory of monomer mon into an array.  The folder in path is
-    assumed to have the openmmlib generated structure of block*.dat
-    """
-    filenames = openmmlib.polymerutils.scanBlocks(path)["files"]
-
-    # Extract trajectory of the pulled monomer
-    traj = np.zeros((len(filenames), 3))
-    for i in range(len(filenames)):
-        traj[i, :] = openmmlib.polymerutils.load(filenames[i])[mon, :]
-    
-    return traj
+    rgb = [(round(255*col[0]), round(255*col[1]), round(255*col[2])) for col in map(cmap, np.arange(N)/(N-1))]
+    return [0x010000*col[0] + 0x000100*col[1] + 0x000001*col[2] for col in rgb]
 
 # ------------ Displaying polymers with nglview -------------
 
-def mdtop_for_polymer(N, exclude_bonds=np.array([])):
+def mdtop_for_polymer(N, exclude_bonds=np.array([]), add_bonds=np.array([]), chains=[(0, None, False)], atom_names="XXX"):
     """
     Generate an mdtraj.Topology object for a single polymer of length N.
-    Optional arguments:
-        - exclude_bonds = [2, 5, 5788]: bonds to not include in the topology. Number refers
-                                        to the second atom of the bond, i.e. atoms numbers
-                                        are 0---(N-1), bond numbers are 1---(N-1)
-          Use case: visualize compartments. nglview has large memory overhead for visualizing
-                    many short chains, so put all compartments into one chain and exclude the
-                    bonds between them.
+
+    Parameters
+    ----------
+    N : int
+        total number of monomers in the polymer, only mandatory argument.
+    chains : list of 3-tuples (polychrom chain specification)
+        chain specification, in the format (first_mon, last_mon, ring?), just
+        as polychrom does. Note that every atom has to belong to exactly one
+        chain, thus the chains have to be contiguous.
+    exclude_bonds : array-like, (any)x1
+        list of chain bonds to exclude from the topology. The bonds are
+        numbered with the second atom in it, i.e. the bond between atom 59 and
+        atom 60 has the number 60.
+    add_bonds : array-like, (any)x2
+        list of additional bonds to include. Every entry is read as two atoms
+        that are to be bound together.
+    atom_names : string (up to 3 characters) or list of such
+        names for the atoms. This can be useful for labelling different types
+        of monomers, such as compartments. If given as list, should have length
+        N.
+
+    Notes
+    -----
+        - use iron as element to prevent NGLView from calculating bonds
+        - also don't try to make NGLView detect this as backbone, because then
+          it would try to calculate bonds again (even though this would be nice
+          for some representations)
+        - we can exploit the way NGLView displays the residue name and number
+          to give a seven digit atom number
     """
+    # Check that chains are good
+    chains.sort()
+    chains = np.array(chains)
+    if not all(chains[:, 0] == [0, *chains[:-1, 1]]):
+        print("WARNING: chains should be contiguous! Using one long chain.")
+        chains = np.array([(0, N, 0)])
+    if chains[-1, 1] is None:
+        chains[-1, 1] = N
+
+    # Check that atom names are good
+    if isinstance(atom_names, str):
+        atom_names = [atom_names[:3] for _ in range(N)]
+    else if not isinstance(atom_names, list) or not len(atom_names) == N:
+        print("WARNING: atom_names should be a list of length N = {}. Using default." % N)
+        atom_names = ["XXX" for _ in range(N)]
+
+    # Generate topology
     top = mdtraj.Topology()
-    ch = top.add_chain()
-    res = top.add_residue("000", ch)
-    top.add_atom('at0', mdtraj.element.helium, res)
-    for i in range(1, N):
-        res = top.add_residue("{0:03d}".format(i%1000), ch)
-        atom = top.add_atom("at{0}".format(i), mdtraj.element.helium, res)
-        if not i in exclude_bonds:
-            top.add_bond(atom, top.atom(i-1))
+    resi = 0
+    for chspec in chains:
+        ch = top.add_chain()
+        res = top.add_residue("{0:03d}".format(int(np.floor(resi/1000))), ch)
+        first_atom = top.add_atom(atom_names[resi], mdtraj.element.iron, res)
+        resi += 1
+        prev_atom = first_atom
+        for _ in range(chspec[0]+1, chspec[1]):
+            res = top.add_residue("{0:03d}".format(int(np.floor(resi/1000))), ch)
+            cur_atom = top.add_atom(atom_names[resi], mdtraj.element.iron, res)
+            resi += 1
+            if not resi in exclude_bonds:
+                top.add_bond(cur_atom, prev_atom)
+            prev_atom = cur_atom
+        if chspec[2]:
+            top.add_bond(cur_atom, first_atom)
+
+    for bond in add_bonds:
+        top.add_bond(top.atom(bond[0]), top.atom(bond[1]))
+
     return top
 
-def xyz2mdtraj(xyz_array, top=None, exclude_bonds=np.array([])):
+def xyz2mdtraj(xyz_array, top=None):
     """
     Generate an mdtraj.Trajectory from a numpy array with coordinates.  This is
     done via transient writing of an .xyz file.
@@ -122,8 +136,11 @@ def xyz2mdtraj(xyz_array, top=None, exclude_bonds=np.array([])):
         atoms.
     top : mdtraj.Topology
         topology of the chain. If omitted, a single long chain is assumed.
-    exclude_bonds : list of bonds to exclude. See mdtop_for_polymer. Ignored if
-        top is given
+
+    Notes
+    -----
+    This used to take an excluded_bonds argument as well. This use is
+    discouraged now. Generate the topology yourself, using mdtop_for_polymer.
     """
     if len(xyz_array.shape) == 2:
         xyz_array = np.expand_dims(xyz_array, 0)
@@ -131,8 +148,9 @@ def xyz2mdtraj(xyz_array, top=None, exclude_bonds=np.array([])):
         error('xyz_array has to have shape (n_atoms, 3) or (n_frames, n_atoms, 3)!')
     
     if top is None:
-        top = mdtop_for_polymer(xyz_array.shape[1], exclude_bonds=exclude_bonds)
+        top = mdtop_for_polymer(xyz_array.shape[1])
      
+    # use tempdir instead of file, because we want mdtraj to create the file
     with tempfile.TemporaryDirectory() as tmpdir:
         filename = os.path.join(tmpdir, 'poly.xyz')
         with mdtraj.formats.XYZTrajectoryFile(filename, mode='w') as file:
@@ -151,14 +169,20 @@ def mdtraj2nglview(mdtrajs, cmaps=[plt.cm.viridis]):
     mdtrajs : list of mdtraj.Trajectory
     cmaps : list of colormaps
     """
+    if not isinstance(cmaps, list):
+        cmaps = [cmaps]
+
     traj = mdtrajs[0]
     for i in range(1, len(mdtrajs)):
         traj = traj.stack(mdtrajs[i])
     
     view = nglview.show_mdtraj(traj)
     view.clear_representations()
-    view.add_representation('licorice')
-    view._set_color_by_residue(hexcolors_polymers([t.n_residues for t in mdtrajs], cmaps))
+    for i in range(len(mdtrajs)):
+        # add representations for all trajs
+        view.add_representation('licorice', selection=':{0}'.format(chr(65+i)),
+                                            colorScheme='atomindex',
+                                            colorScale=hexscale_from_cmap(cmaps[i], mdtrajs[i].n_atoms))
     
     return view
 
